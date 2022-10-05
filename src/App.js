018a59e3214ba34fb5b08c94b39406c9e99e5920
Vladyslav <65622620+Naykitin@users.noreply.github.com>
2022-10-05 12:46:37 +0300

    install sass

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Skills</li>
+            <li>Work</li>
+          </ul>
+        </nav>
       </header>
     </div>
   );
